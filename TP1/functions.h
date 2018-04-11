28a57bb4031aacf13489618a95944d85fbb8b5db
esquivaflechas <23452482+esquivaflechas@users.noreply.github.com>
2018-04-11 00:25:38 -0300

    Add files via upload

--- TP1/functions.h
+++ TP1/functions.h
@@ -1,5 +1,47 @@
-int suma(int numero1, int numero2);
-int resta(int numero1, int numero2);
-int multiplicacion(int numero1, int numero2);
-int division(int numero1, int numero2);
-int factorial(int numero);
+/** \brief Suma dos numeros entre ellos
+ *
+ * \param primer numero
+ * \param segundo numero
+ * \return el resultado de la suma
+ *
+ */
+int suma(int , int );
+
+/** \brief resta el segundo operador al primero
+ *
+ * \param el minuendo de la resta
+ * \param el substraendo de la resta
+ * \return el resultado de la resta
+ *
+ */
+
+int resta(int , int );
+
+/** \brief Multiplica dos numeros entre si
+ *
+ * \param El primer numero que se multiplica
+ * \param El segundo numero que se multiplica
+ * \return El resultado de la multiplicacion
+ *
+ */
+
+int multiplicacion(int , int );
+
+/** \brief Se divide el primer operador por el segundo
+ *
+ * \param el divisor de la cuenta
+ * \param el dividendo de la cuenta
+ * \return El resultado de la division entre los dos operadores
+ *
+ */
+
+int division(int , int );
+
+/** \brief Realiza el factorial del parametro entregado
+ *
+ * \param Numero que se va a factorizar
+ * \return El resultado del factorial del numero
+ *
+ */
+
+int factorial(int );
